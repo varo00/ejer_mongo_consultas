95322160fb040cde2d927b0e82e76e839f0f46bc
Alvaro Gomez Garcia <44815166+varo00@users.noreply.github.com>
2024-12-03 11:16:39 +0100

    done problema 9, todos los enunciados copiados

--- mongo_practicas.js
+++ mongo_practicas.js
@@ -212,6 +212,20 @@ db.videojuegos.find({ "rating": { $gt: 9.0 } }).sort({ "releaseYear": -1 })
 // 4.Encuentra todos los videojuegos que tengan una calificación mayor a 8.7 y que pertenezcan al género "Adventure".
 db.videojuegos.find({ $and: [{ "rating": { $gt: 8.7 } }, { "genre": "Adventure" }] })
 // 5.Encuentra el videojuego con el título más largo en la colección
+db.videojuegos.aggregate([
+    {
+        $project: {
+            title: 1,
+            titleLength: { $strLenCP: "$title" }
+        }
+    },
+    {
+        $sort: { titleLength: -1 }
+    },
+    {
+        $limit: 1
+    }
+])
 // 6.Encuentra todos los videojuegos lanzados en o después de 2017
 db.videojuegos.find({ "releaseYear": { $gte: 2017 } })
 // 7.Encuentra dos videojuegos cuyo título comience con la letra "T"
@@ -301,18 +315,186 @@ db.users.find()
 // 2.Encuentra todas las publicaciones.
 db.posts.find()
 // 3.Encuentra todas las publicaciones escritas por "SuperCoder123".
-db.posts.aggregate([{$match: {username: "SuperCoder123"}}])
+db.posts.aggregate([{ $match: { username: "SuperCoder123" } }])
 // 4.Encuentra todas las publicaciones escritas por "TechGuru99".
-db.posts.aggregate([{$match: {username: "TechGuru99"}}])
+db.posts.aggregate([{ $match: { username: "TechGuru99" } }])
 // 5.Encuentra todos los comentarios.
 db.comments.find()
 // 6.Encuentra todos los comentarios escritos por "SuperCoder123".
-db.comments.aggregate([{$match: {username: "SuperCoder123"}}])
+db.comments.aggregate([{ $match: { username: "SuperCoder123" } }])
 // 7.Encuentra todos los comentarios escritos por "TechGuru99".
-db.comments.aggregate([{$match: {username: "TechGuru99"}}])
+db.comments.aggregate([{ $match: { username: "TechGuru99" } }])
 // 8.Encuentra todos los comentarios pertenecientes a la publicación “Shares coding tutorials"
 db.comments.aggregate([
-    { $lookup: { from:"posts", localField:"post", foreignField:"_id", as:"post_info" } },
+    { $lookup: { from: "posts", localField: "post", foreignField: "_id", as: "post_info" } },
     { $unwind: "$post_info" },
-    { $match: { "post_info.title":"Shares coding tutorials" } }
+    { $match: { "post_info.title": "Shares coding tutorials" } }
 ])
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Problema 7: Consultas avanzadas
+// 1.Encuentra todos los videojuegos cuyo título contiene la palabra 'Legend'.
+// Encuentra los videojuegos cuyo título termine con la letra 'e'.
+// Ordena los videojuegos encontrados por el año de lanzamiento en orden descendente.
+db.videojuegos.find({
+    $or: [
+        { title: /Legend/ }, // que contenga la palabra Legend
+        { title: /e$/ } // que termine en e
+    ]
+}).sort({ releaseYear: -1 })
+// 2.Encuentra todos los videojuegos con más de dos géneros.
+// Filtra los videojuegos que tengan más de tres plataformas.
+// Encuentra el videojuego con más géneros en su lista.
+db.videojuegos.aggregate([
+    { $unwind: "$genre" },
+    { $group: { _id: "$title", genres: { $addToSet: "$genre" } } },
+    { $match: { $expr: { $gt: [{ $size: "$genres" }, 2] } } }
+])
+// 3.Encuentra videojuegos cuya plataforma incluye 'PlayStation' y 'PC'.
+// Encuentra videojuegos que tengan exactamente estas dos plataformas.
+// Ordena los resultados por calificación en orden descendente.
+db.videojuegos.aggregate([
+    { $match: { platform: { $all: ["PlayStation", "PC"] } } },
+    { $unwind: "$platform" },
+    { $group: { _id: "$_id", platforms: { $addToSet: "$platform" }, rating: { $first: "$rating" } } },
+    { $match: { $expr: { $eq: [{ $size: "$platforms" }, 2] } } },
+    { $sort: { rating: -1 } }
+])
+// 4.Encuentra videojuegos lanzados después de 2015 que sean de género 'Action' o 'RPG'.
+// Encuentra cuántos videojuegos cumplen esta condición.
+db.videojuegos.find({
+    $and: [
+        { releaseYear: { $gt: 2015 } },
+        { genre: { $in: ["Action", "RPG"] } }
+    ]
+}).count()
+// Calcula el promedio de calificaciones para estos videojuegos.
+db.videojuegos.aggregate([
+    { $match: { $and: [{ releaseYear: { $gt: 2015 } }, { genre: { $in: ["Action", "RPG"] } }] } },
+    { $group: { _id: null, avgRating: { $avg: "$rating" } } }
+])
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Problema 8: Operaciones de agrupación y agregación
+// 1.Encuentra el promedio de calificaciones y el total de videojuegos por género.
+db.videojuegos.aggregate([
+    { $unwind: "$genre" },
+    { $group: { _id: "$genre", totalVideojuegos: { $sum: 1 }, promedioCalificaciones: { $avg: "$rating" } } },
+    { $project: { _id: 0, genero: "$_id", totalVideojuegos: 1, promedioCalificaciones: { $round: ["$promedioCalificaciones", 2] } } }
+])
+// 2.Calcula también el año más reciente de lanzamiento por género.
+db.videojuegos.aggregate([
+    { $unwind: "$genre" },
+    { $group: { _id: "$genre", latestReleaseYear: { $max: "$releaseYear" } } },
+    { $project: { _id: 0, genre: "$_id", latestReleaseYear: 1 } }
+])
+// 3.Encuentra los géneros con un promedio de calificación superior a 9.0.
+db.videojuegos.aggregate([
+    { $unwind: "$genre" },
+    { $group: { _id: "$genre", avgRating: { $avg: "$rating" } } },
+    { $match: { avgRating: { $gt: 9 } } },
+    { $project: { _id: 0, genre: "$_id", avgRating: { $round: ["$avgRating", 2] } } }
+])
+// 4.Supón que cada videojuego se vende 1,000 veces. Calcula el ingreso total por plataforma.
+db.videojuegos.aggregate([
+    { $unwind: "$platform" },
+    { $group: { _id: "$platform", totalIngresos: { $sum: { $multiply: ["$rating", 1000] } } } },
+    { $sort: { totalIngresos: -1 } }
+])
+// 5.Encuentra la plataforma que genera los mayores ingresos.
+db.videojuegos.aggregate([
+    { $unwind: "$platform" },
+    { $group: { _id: "$platform", totalIngresos: { $sum: { $multiply: ["$rating", 1000] } } } },
+    { $sort: { totalIngresos: -1 } },
+    { $limit: 1 }
+])
+// 6.Calcula también el promedio de ingresos por plataforma.
+db.videojuegos.aggregate([
+    { $unwind: "$platform" },
+    { $group: { _id: "$platform", avgIngresos: { $avg: { $multiply: ["$rating", 1000] } } } },
+    { $sort: { avgIngresos: -1 } }
+])
+// 7.Combina las colecciones `videojuegos` y `users` para encontrar los nombres de usuarios que compraron juegos de género 'RPG'.
+db.videojuegos.aggregate([
+    { $match: { genre: "RPG" } },
+    { $lookup: { from: "users", localField: "username", foreignField: "username", as: "user_info" } },
+    { $unwind: "$user_info" },
+    { $project: { _id: 0, username: "$user_info.username" } }
+])
+// 8.Encuentra también los usuarios que compraron videojuegos con calificación mayor a 9.0.
+db.videojuegos.aggregate([
+    { $match: { rating: { $gt: 9 } } },
+    { $lookup: { from: "users", localField: "username", foreignField: "username", as: "user_info" } },
+    { $unwind: "$user_info" },
+    { $project: { _id: 0, username: "$user_info.username" } }
+])
+// 9.Genera un listado de usuarios con los títulos de los videojuegos que han comprado.
+db.videojuegos.aggregate([
+    { $lookup: { from: "users", localField: "username", foreignField: "username", as: "user_info" } },
+    { $unwind: "$user_info" },
+    { $project: { _id: 0, username: "$user_info.username", title: "$title" } }
+])
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Problema 9: Uso de índices
+// 1.Crea un índice compuesto en `title` (texto) y `releaseYear` (descendente).
+db.videojuegos.createIndex({ title: "text", releaseYear: -1 })
+// 2.Realiza una consulta que utilice este índice y analiza su rendimiento con `explain()`.
+db.videojuegos.find(
+    { $text: { $search: "League" }, releaseYear: { $gt: 2010 } }
+).explain("executionStats")
+// 3.Verifica si este índice mejora las consultas que filtran por ambos campos.
+db.videojuegos.find(
+    { $text: { $search: "League" }, releaseYear: { $gt: 2010 } }
+).explain("executionStats")
+// 4.Usa el método `explain()` para analizar el uso del índice en una consulta que filtre videojuegos por palabras clave en el título.
+db.videojuegos.find({ $text: { $search: "Legend Zelda" } }).explain()
+// 5.Prueba la consulta con y sin el índice.
+db.videojuegos.find({ title: "The Legend of Zelda: Breath of the Wild" }).explain()
+db.videojuegos.dropIndex("title_text_releaseYear_-1")
+db.videojuegos.find(
+    { $text: { $search: "League" }, releaseYear: { $gt: 2010 } }
+).explain("executionStats")
+// 6.Compara el tiempo de ejecución y el número de documentos examinados.
+// 7.Crea un índice parcial para incluir solo los videojuegos con calificación mayor a 9.0.
+db.videojuegos.createIndex({ rating: 1 }, { partialFilterExpression: { rating: { $gt: 9 } } })
+// 8.Realiza una consulta que filtre videojuegos con calificación mayor a 9.0 y analiza su rendimiento.
+db.videojuegos.find({ rating: { $gt: 9 } }).explain("executionStats")
+// 9.Encuentra los videojuegos con el índice parcial que pertenezcan al género 'Adventure'.
+db.videojuegos.find({ genre: "Adventure", rating: { $gt: 9 } }).explain("executionStats")
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Problema 10: Relaciones complejas
+// 1.Inserta una nueva compra para un usuario existente.
+// 2.Encuentra todos los usuarios que han realizado compras.
+// 3.Actualiza el historial de compras de un usuario para incluir un nuevo videojuego.
+// 4.Inserta más comentarios relacionados con los posts de los usuarios.
+// 5.Encuentra todos los comentarios realizados por un usuario específico.
+// 6.Cuenta el número total de comentarios por usuario.
+// 7.Encuentra todas las publicaciones con sus respectivos comentarios.
+// 8.Encuentra las publicaciones con más de dos comentarios.
+// 9.Ordena las publicaciones por el número de comentarios de forma descendente.
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Problema 11: Persistencia
+// 1.Persiste los resultados de videojuegos agrupados por género en una nueva colección llamada `genre_analysis`.
+// 2.Añade también el campo de género con el número de plataformas promedio por género.
+// 3.Encuentra los géneros que tienen más de cinco videojuegos y persiste solo esos resultados.
+// 4.Exporta la colección `users` en formato JSON.
+// 5.Exporta también la colección `series` en formato JSON.
+// 6.Genera un archivo JSON con los videojuegos que tienen calificación superior a 9.0.
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Problema 12: Ejecución de scripts
+// 1.Documenta todas las consultas realizadas en un archivo `mongo_practica.js` para ejecutarlas directamente desde la shell.
+// 2.Divide las consultas en secciones según el problema que resuelven.
+// 3.Incluye comentarios en cada consulta explicando su propósito.
+// 4.Crea una función en JavaScript que permita buscar videojuegos según condiciones específicas pasadas como parámetro.
+// 5.Extiende la función para incluir un parámetro opcional que ordene los resultados.
+// 6.Agrega un límite de resultados en la función para devolver solo los primeros `n` documentos encontrados.
